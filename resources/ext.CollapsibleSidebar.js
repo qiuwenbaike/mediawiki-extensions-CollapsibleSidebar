4f99ad422232adb5921c5b835976cdb28a3c0546
WaitSpring <me@waitspring.com>
2023-07-13 10:52:58 +0800

    fix: remove pointless codes
    Signed-off-by: WaitSpring <me@waitspring.com>

--- resources/ext.CollapsibleSidebar.js
+++ resources/ext.CollapsibleSidebar.js
@@ -14,29 +14,6 @@
 	if ( ![ 'vector', 'gongbi', 'timeless', 'write' ].includes( mw.config.get( 'skin' ) ) ) {
 		return;
 	}
-	const getCookie = ( name ) => ( '; '
-		.concat( decodeURIComponent( document.cookie ) )
-		.split( '; '.concat( name, '=' ) )
-		.pop()
-		.split( ';' )
-		.shift() );
-	const setCookie = ( name, value, time, path = '/', isSecure = true ) => {
-		if ( !name || !value || !time || !path ) {
-			return;
-		}
-		const base = ''
-			.concat( name, '=' )
-			.concat( encodeURIComponent( value ), ';path=' )
-			.concat( path )
-			.concat( isSecure ? ';Secure' : '' );
-		const date = new Date();
-		if ( time === 'tmp' ) {
-			document.cookie = base;
-		} else {
-			date.setTime( date.getTime() + time * 36e5 );
-			document.cookie = ''.concat( base, ';expires=' ).concat( date.toGMTString() );
-		}
-	};
 	const cookieName = 'usecollapsedsidebar';
 	const images = {
 		hideSidebarButtonIcon: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.615' cy='6.615' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M3.307 3.307v2.205h.735v-1.47h1.47v-.735zm.735 4.41h-.735v2.205h2.205v-.735h-1.47zm5.145 1.47h-1.47v.735h2.205V7.717h-.735zm0-5.88h-1.47v.735h1.47v1.47h.735V3.307z' fill='%2336c'/%3E%3C/svg%3E",
@@ -81,23 +58,23 @@
 	const switchMode = {
 		hide: () => {
 			document.documentElement.classList.add( 'client-collapsedsidebar' );
-			setCookie( cookieName, '0', '-1' );
-			setCookie( cookieName, '1', 1e9 );
+			window.setCookie( cookieName, '0', '-1' );
+			window.setCookie( cookieName, '1', 1e9 );
 			sidebarButton.src = images.showSidebarButtonIcon;
 			sidebarButton.alt = mw.message( 'collapsiblesidebar-show-link' );
 			sidebarButton.title = mw.message( 'collapsiblesidebar-show-link-tooltip' );
 		},
 		show: () => {
 			document.documentElement.classList.remove( 'client-collapsedsidebar' );
-			setCookie( cookieName, '1', '-1' );
-			setCookie( cookieName, '0', 1e9 );
+			window.setCookie( cookieName, '1', '-1' );
+			window.setCookie( cookieName, '0', 1e9 );
 			sidebarButton.src = images.hideSidebarButtonIcon;
 			sidebarButton.alt = mw.message( 'collapsiblesidebar-collapse-link' );
 			sidebarButton.title = mw.message( 'collapsiblesidebar-collapse-link-tooltip' );
 		}
 	};
 	const checkSidebar = () => {
-		if ( getCookie( cookieName ) === '' ) {
+		if ( window.getCookie( cookieName ) === '' ) {
 			if ( isSidebarCollapsed ) {
 				switchMode.hide();
 			} else {
@@ -106,10 +83,10 @@
 		}
 	};
 	const modeSwitcher = () => {
-		if ( getCookie( cookieName ) === '' ) {
+		if ( window.getCookie( cookieName ) === '' ) {
 			checkSidebar();
 		}
-		if ( getCookie( cookieName ) === '0' ) {
+		if ( window.getCookie( cookieName ) === '0' ) {
 			switchMode.hide();
 		} else {
 			switchMode.show();
