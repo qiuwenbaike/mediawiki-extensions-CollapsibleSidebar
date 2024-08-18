02cc960d29ad8f04d68e67c9a5eeaa225b745562
WaitSpring <me@waitspring.com>
2024-08-18 20:34:58 +0800

    rewrite in es3

--- resources/ext.CollapsibleSidebar.js
+++ resources/ext.CollapsibleSidebar.js
@@ -6,25 +6,27 @@
  */
 
 (function () {
-	const { wgCanonicalSpecialPageName, skin } = mw.config.get();
-
+	var wgCanonicalSpecialPageName = mw.config.get(
+		'wgCanonicalSpecialPageName'
+	);
+	var skin = mw.config.get('skin');
 	if (wgCanonicalSpecialPageName === 'ApiHelp' || skin === 'vector-2022') {
 		return;
 	}
 
-	const COOKIE_NAME = 'usecollapsedsidebar',
-		HIDE_ICON =
-			"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.615' cy='6.615' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M3.307 3.307v2.205h.735v-1.47h1.47v-.735zm.735 4.41h-.735v2.205h2.205v-.735h-1.47zm5.145 1.47h-1.47v.735h2.205V7.717h-.735zm0-5.88h-1.47v.735h1.47v1.47h.735V3.307z' fill='%2336c'/%3E%3C/svg%3E",
-		SHOW_ICON =
-			"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.615' cy='6.615' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M5.512 5.512V3.307h-.735v1.47h-1.47v.735zm-.735 4.41h.735V7.717H3.307v.735h1.47zm3.675-1.47h1.47v-.735H7.717v2.205h.735zm0-2.94h1.47v-.735h-1.47v-1.47h-.735v2.205z' fill='%2336c'/%3E%3C/svg%3E",
-		IS_COLLAPSED = document.documentElement.classList.contains(
-			'client-collapsedsidebar'
-		),
-		message = function (key) {
-			return mw.message('collapsiblesidebar-' + key).plain();
-		};
+	var COOKIE_NAME = 'usecollapsedsidebar';
+	var HIDE_ICON =
+		"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.615' cy='6.615' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M3.307 3.307v2.205h.735v-1.47h1.47v-.735zm.735 4.41h-.735v2.205h2.205v-.735h-1.47zm5.145 1.47h-1.47v.735h2.205V7.717h-.735zm0-5.88h-1.47v.735h1.47v1.47h.735V3.307z' fill='%2336c'/%3E%3C/svg%3E";
+	var SHOW_ICON =
+		"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.615' cy='6.615' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M5.512 5.512V3.307h-.735v1.47h-1.47v.735zm-.735 4.41h.735V7.717H3.307v.735h1.47zm3.675-1.47h1.47v-.735H7.717v2.205h.735zm0-2.94h1.47v-.735h-1.47v-1.47h-.735v2.205z' fill='%2336c'/%3E%3C/svg%3E";
+	var IS_COLLAPSED = document.documentElement.classList.contains(
+		'client-collapsedsidebar'
+	);
+	var message = function (key) {
+		return mw.message('collapsiblesidebar-' + key).plain();
+	};
 
-	const button = document.createElement('img');
+	var button = document.createElement('img');
 	button.id = 'sidebarButton';
 	button.src = IS_COLLAPSED ? SHOW_ICON : HIDE_ICON;
 	button.draggable = false;
@@ -35,14 +37,14 @@
 	button.style.opacity = '0.7';
 	button.style.bottom = '169px';
 	/* add EventListener to button */
-	const eventTargetFunction = function (event) {
+	var eventTargetFunction = function (event) {
 		button.style.opacity = event.type === 'mouseenter' ? '1' : '0.7';
 	};
 	button.addEventListener('mouseenter ', eventTargetFunction);
 	button.addEventListener('mouseleave', eventTargetFunction);
 	document.body.appendChild(button);
 
-	const eventFunc = function () {
+	var eventFunc = function () {
 		button.style.bottom =
 			document.getElementById('cat_a_lot') ||
 			document.getElementById('proveit') ||
@@ -53,7 +55,7 @@
 	window.addEventListener('scroll', eventFunc);
 	window.addEventListener('selectionchange', eventFunc);
 
-	const getCookie = function (name) {
+	var getCookie = function (name) {
 		return '; '
 			.concat(decodeURIComponent(document.cookie))
 			.split('; '.concat(name, '='))
@@ -62,24 +64,24 @@
 			.shift();
 	};
 
-	const setCookie = function (object) {
-		const name = object.name,
-			value = object.value,
-			hour = object.hour || 0,
-			path = object.path || '/',
-			isSecure = object.isSecure || true;
+	var setCookie = function (object) {
+		var name = object.name;
+		var value = object.value;
+		var hour = object.hour || 0;
+		var path = object.path || '/';
+		var isSecure = object.isSecure || true;
 
 		if (!name || !value || !path) {
 			return;
 		}
 
-		const base = ''
+		var base = ''
 			.concat(name, '=')
 			.concat(encodeURIComponent(value), ';path=')
 			.concat(path)
 			.concat(isSecure ? ';Secure' : '');
 
-		const date = new Date();
+		var date = new Date();
 
 		if (hour === 0) {
 			document.cookie = base;
@@ -91,7 +93,7 @@
 		}
 	};
 
-	const switchMode = {
+	var switchMode = {
 		hide: function () {
 			document.documentElement.classList.add('client-collapsedsidebar');
 			if (skin === 'write') {
@@ -128,7 +130,7 @@
 		}
 	};
 
-	const checkSidebar = function () {
+	var checkSidebar = function () {
 		if (getCookie(COOKIE_NAME) === '') {
 			if (IS_COLLAPSED) {
 				switchMode.hide();
@@ -138,7 +140,7 @@
 		}
 	};
 
-	const modeSwitcher = function () {
+	var modeSwitcher = function () {
 		if (getCookie(COOKIE_NAME) === '') {
 			checkSidebar();
 		}
@@ -149,7 +151,7 @@
 		}
 	};
 
-	button.addEventListener('click', () => {
+	button.addEventListener('click', function () {
 		modeSwitcher();
 	});
 
