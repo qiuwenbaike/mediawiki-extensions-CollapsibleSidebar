eab9f04c63329e67d7f949037118587682b9a056
WaitSpring <me@waitspring.com>
2025-02-13 11:58:56 +0800

    format

--- resources/ext.CollapsibleSidebar.js
+++ resources/ext.CollapsibleSidebar.js
@@ -6,27 +6,27 @@
  */
 
 (function () {
-	var wgCanonicalSpecialPageName = mw.config.get(
+	const wgCanonicalSpecialPageName = mw.config.get(
 		'wgCanonicalSpecialPageName'
 	);
-	var skin = mw.config.get('skin');
+	const skin = mw.config.get('skin');
 	if (wgCanonicalSpecialPageName === 'ApiHelp' || skin === 'vector-2022') {
 		return;
 	}
 
-	var COOKIE_NAME = 'usecollapsedsidebar';
-	var HIDE_ICON =
+	const COOKIE_NAME = 'usecollapsedsidebar';
+	const HIDE_ICON =
 		"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.615' cy='6.615' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M3.307 3.307v2.205h.735v-1.47h1.47v-.735zm.735 4.41h-.735v2.205h2.205v-.735h-1.47zm5.145 1.47h-1.47v.735h2.205V7.717h-.735zm0-5.88h-1.47v.735h1.47v1.47h.735V3.307z' fill='%2336c'/%3E%3C/svg%3E";
-	var SHOW_ICON =
+	const SHOW_ICON =
 		"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.615' cy='6.615' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M5.512 5.512V3.307h-.735v1.47h-1.47v.735zm-.735 4.41h.735V7.717H3.307v.735h1.47zm3.675-1.47h1.47v-.735H7.717v2.205h.735zm0-2.94h1.47v-.735h-1.47v-1.47h-.735v2.205z' fill='%2336c'/%3E%3C/svg%3E";
-	var IS_COLLAPSED = document.documentElement.classList.contains(
+	const IS_COLLAPSED = document.documentElement.classList.contains(
 		'client-collapsedsidebar'
 	);
-	var message = function (key) {
+	const message = function (key) {
 		return mw.message('collapsiblesidebar-' + key).plain();
 	};
 
-	var button = document.createElement('img');
+	const button = document.createElement('img');
 	button.id = 'sidebarButton';
 	button.src = IS_COLLAPSED ? SHOW_ICON : HIDE_ICON;
 	button.draggable = false;
@@ -37,14 +37,14 @@
 	button.style.opacity = '0.7';
 	button.style.bottom = '169px';
 	/* add EventListener to button */
-	var eventTargetFunction = function (event) {
+	const eventTargetFunction = function (event) {
 		button.style.opacity = event.type === 'mouseenter' ? '1' : '0.7';
 	};
 	button.addEventListener('mouseenter ', eventTargetFunction);
 	button.addEventListener('mouseleave', eventTargetFunction);
 	document.body.appendChild(button);
 
-	var eventFunc = function () {
+	const eventFunc = function () {
 		button.style.bottom =
 			document.getElementById('cat_a_lot') ||
 			document.getElementById('proveit') ||
@@ -55,7 +55,7 @@
 	window.addEventListener('scroll', eventFunc);
 	window.addEventListener('selectionchange', eventFunc);
 
-	var getCookie = function (name) {
+	const getCookie = function (name) {
 		return '; '
 			.concat(decodeURIComponent(document.cookie))
 			.split('; '.concat(name, '='))
@@ -64,24 +64,24 @@
 			.shift();
 	};
 
-	var setCookie = function (object) {
-		var name = object.name;
-		var value = object.value;
-		var hour = object.hour || 0;
-		var path = object.path || '/';
-		var isSecure = object.isSecure || true;
+	const setCookie = function (object) {
+		const name = object.name;
+		const value = object.value;
+		const hour = object.hour || 0;
+		const path = object.path || '/';
+		const isSecure = object.isSecure || true;
 
 		if (!name || !value || !path) {
 			return;
 		}
 
-		var base = ''
+		const base = ''
 			.concat(name, '=')
 			.concat(encodeURIComponent(value), ';path=')
 			.concat(path)
 			.concat(isSecure ? ';Secure' : '');
 
-		var date = new Date();
+		const date = new Date();
 
 		if (hour === 0) {
 			document.cookie = base;
@@ -93,7 +93,7 @@
 		}
 	};
 
-	var switchMode = {
+	const switchMode = {
 		hide: function () {
 			document.documentElement.classList.add('client-collapsedsidebar');
 			if (skin === 'write') {
@@ -130,7 +130,7 @@
 		}
 	};
 
-	var checkSidebar = function () {
+	const checkSidebar = function () {
 		if (getCookie(COOKIE_NAME) === '') {
 			if (IS_COLLAPSED) {
 				switchMode.hide();
@@ -140,7 +140,7 @@
 		}
 	};
 
-	var modeSwitcher = function () {
+	const modeSwitcher = function () {
 		if (getCookie(COOKIE_NAME) === '') {
 			checkSidebar();
 		}
@@ -151,7 +151,7 @@
 		}
 	};
 
-	button.addEventListener('click', function () {
+	button.addEventListener('click', () => {
 		modeSwitcher();
 	});
 
